515d486324ec9b81395453eabaae5ca80da079af
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-14 23:38:11 +0600

    import firebase neccessary file and add a button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Sign In</button>
     </div>
   );
 }
