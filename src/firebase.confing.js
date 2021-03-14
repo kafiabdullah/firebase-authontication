86c25b86668f47f72e69e62603a1362f23cf6bab
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-14 23:05:42 +0600

    add firebase authontication configure file

---
+++ src/firebase.confing.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCuLhN89VLYQbWtGrKDNUsIy6m_FkbU4Pk",
+    authDomain: "ema-john-simple-3de66.firebaseapp.com",
+    projectId: "ema-john-simple-3de66",
+    storageBucket: "ema-john-simple-3de66.appspot.com",
+    messagingSenderId: "663237959680",
+    appId: "1:663237959680:web:c853ae1b3ae96d1836b8e5"
+  };
+
+
+export default firebaseConfig; 
